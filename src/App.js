5310068f87f9f2c9c3db43090d282d7087d39456
anuj-jft <anuj.singh@jellyfishtechnologies.com>
2023-10-05 11:04:19 +0530

    donation, register form created and adjustable footer

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ import Homepage from './pages/homepage';
 import AboutUs from './pages/about';
 import LatestCause from './pages/cause';
 import { Container } from '@chakra-ui/react';
+import PageNotFound from './pages/page-not-found';
+import SignUpComponent from './pages/register';
+import DonationForm from './pages/donation';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Container maxW={'100vw'} overflowX={'hidden'} p='0px' m='0px'>
+      <Container maxW={'100vw'} overflowX={'hidden'} p='0px' m='0px' minH={'90vh'}>
         <Navbar />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/about' element={<AboutUs />} />
           <Route path='/latest-cause' element={<LatestCause />} />
+          <Route path='/contact' element={<SignUpComponent />} />
+          <Route path='/donation' element={<DonationForm />} />
+          <Route path='*' element={<PageNotFound />} />
         </Routes>
         <Footer />
       </Container>
